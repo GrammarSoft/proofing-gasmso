27f2335ee726eb233925614179597dd220a6176b
Tino Didriksen <mail@tinodidriksen.com>
2017-11-29 11:38:12 +0000

    Initial import GAS
    git-svn-id: file:///home/komma/repo-gas/svn@1 8336fc70-c169-11e8-9c4d-f55cbf993aad

---
+++ mv-nordic/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+	"root": true,
+	"extends": "eslint:recommended",
+	"env": {
+		"jquery": true,
+	},
+	"parserOptions": {
+		"ecmaVersion": 5,
+	},
+	"rules": {
+		"block-scoped-var": "error",
+		"block-spacing": "error",
+		"brace-style": ["error", "stroustrup"],
+		"comma-dangle": ["error", "always-multiline"],
+		"eol-last": ["error", "always"],
+		"indent": ["error", "tab"],
+		"linebreak-style": ["error", "unix"],
+		"no-array-constructor": "error",
+		"no-console": "off",
+		"no-constant-condition": "error",
+		"no-duplicate-imports": "error",
+		"no-multiple-empty-lines": "error",
+		"no-new-object": "error",
+		"no-trailing-spaces": "error",
+		"no-use-before-define": ["error", { "functions": false, "classes": true, "variables": true }],
+		"no-var": "error",
+		"prefer-numeric-literals": "error",
+		"semi": ["error", "always"],
+		"strict": ["error", "global"],
+		"unicode-bom": ["error", "never"],
+	},
+};
