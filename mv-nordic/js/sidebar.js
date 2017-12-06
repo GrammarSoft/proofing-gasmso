36534089931c0bc00337b3a971060d01b41d338e
Tino Didriksen <mail@tinodidriksen.com>
2017-12-06 21:24:59 +0000

    Rename files for remote load
    git-svn-id: file:///home/komma/repo-gas/svn@10 8336fc70-c169-11e8-9c4d-f55cbf993aad

--- mv-nordic/js/sidebar.js
+++ mv-nordic/js/sidebar.js
@@ -1,22 +1,21 @@
-<!--
- Copyright 2016-2017 GrammarSoft ApS <info@grammarsoft.com> at https://grammarsoft.com/
- Linguistic backend by Eckhard Bick <eckhard.bick@gmail.com>
- Frontend by Tino Didriksen <mail@tinodidriksen.com>
-
- This project is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This project is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this project.  If not, see <http://www.gnu.org/licenses/>.
--->
-<script>
+/*!
+ * Copyright 2016-2017 GrammarSoft ApS <info@grammarsoft.com> at https://grammarsoft.com/
+ * Linguistic backend by Eckhard Bick <eckhard.bick@gmail.com>
+ * Frontend by Tino Didriksen <mail@tinodidriksen.com>
+ *
+ * This project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this project.  If not, see <http://www.gnu.org/licenses/>.
+ */
 'use strict';
 
 var g_conf = Object.assign({}, g_conf_defaults);
@@ -387,5 +386,3 @@ function showError(msg) {
 	$('#error').show();
 	$('#error-text').text(l10n.t(msg));
 }
-
-</script>
