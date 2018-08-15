5ea56270d756d1cf991f4746331869264b4d14fc
Tino Didriksen <mail@tinodidriksen.com>
2018-08-15 11:19:29 +0000

    Add l10n branch
    git-svn-id: file:///home/komma/repo-gas/svn@96 8336fc70-c169-11e8-9c4d-f55cbf993aad

---
+++ mv-nordic-l10n/js/l10n.js
@@ -0,0 +1,65 @@
+/*!
+ * Copyright 2016-2018 GrammarSoft ApS <info@grammarsoft.com> at https://grammarsoft.com/
+ * Linguistic backend by Eckhard Bick <eckhard.bick@gmail.com>
+ * Frontend by Tino Didriksen <mail@tinodidriksen.com>
+ *
+ * This project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this project.  If not, see <http://www.gnu.org/licenses/>.
+ */
+'use strict';
+
+/* globals session */
+
+/* exported l10n */
+let l10n = {};
+
+l10n.s = {
+	da: {
+		ERR_POSTBACK: 'Der opstod en serverfejl under tjekning af teksten. Prøv evt. igen.',
+		ERR_NO_SELECTION: 'Der var ingen markering. Du skal markere noget tekst før den funktion virker.',
+		ERR_SELECT_NOTFOUND: 'Kunne ikke finde konteksten. Det kan være du eller en anden har ændret i dokumentet siden tjekket blev kørt.',
+		ERR_SELECT_NOMATCH: 'Konteksten matchede ikke. Denne fejl bør ikke kunne ske.',
+		ERR_REPLACE_NOSELECT: 'Kunne ikke vælge stedet i konteksten. Det kan være du eller en anden har ændret i dokumentet siden tjekket blev kørt.',
+		ERR_NO_STORAGE: 'Din browser understøtter ikke HTML5 Local Storage, men dette program fungerer ikke uden Local Storage. Det kan være du har slået det fra eller benytter privat vindue. Slå Local Storage til og prøv igen.',
+		ERR_CADUCEUS_FAILED: 'Kunne ikke oprette en kanal til login. Det kan være du ikke er online eller er bag en proxy eller firewall der blokerer for adgang.',
+		BTN_GRAMMAR_REPLACE: 'Erstat',
+		BTN_GRAMMAR_INSERT: 'Indsæt',
+		BTN_GRAMMAR_REMOVE: 'Fjern',
+		BTN_COMMA_INSERT: 'Indsæt komma',
+		BTN_COMMA_INSERT_STOP: 'Indsæt punktum',
+		BTN_COMMA_REMOVE: 'Fjern komma',
+	},
+	en: {
+		ERR_NO_SELECTION: 'No selection found. You must select some text before that button will work.',
+	},
+};
+
+l10n.t = function(s) {
+	s = '' + s; // Coerce to string
+
+	let l = session.locale;
+	// If the string doesn't exist in the locale, fall back
+	if (!l10n.s[session.locale].hasOwnProperty(s)) {
+		// Try English
+		if (l10n.s.en.hasOwnProperty(s)) {
+			return l10n.s.en[s];
+		}
+		// ...then Danish
+		if (l10n.s.da.hasOwnProperty(s)) {
+			return l10n.s.da[s];
+		}
+		// ...give up and return as-is
+		return s;
+	}
+	return l10n.s[l][s];
+};
---
+++ mv-nordic-l10n/js/options.js
@@ -0,0 +1,257 @@
+/*!
+ * Copyright 2016-2018 GrammarSoft ApS <info@grammarsoft.com> at https://grammarsoft.com/
+ * Linguistic backend by Eckhard Bick <eckhard.bick@gmail.com>
+ * Frontend by Tino Didriksen <mail@tinodidriksen.com>
+ *
+ * This project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this project.  If not, see <http://www.gnu.org/licenses/>.
+ */
+'use strict';
+
+/* globals escHTML */
+/* globals g_conf_defaults */
+/* globals marking_types */
+/* globals google */
+/* globals g_tool:true */
+/* globals l10n */
+
+function saveConfig() {
+	g_conf.opt_ignUnknown = false;
+	if (g_conf.opt_ignUNames && g_conf.opt_ignUComp && g_conf.opt_ignUAbbr && g_conf.opt_ignUOther) {
+		g_conf.opt_ignUnknown = true;
+	}
+	if (g_conf.opt_ignUnknown) {
+		g_conf.opt_ignUNames = g_conf.opt_ignUComp = g_conf.opt_ignUAbbr = g_conf.opt_ignUOther = true;
+	}
+
+	for (let k in g_conf) {
+		if (!g_conf.hasOwnProperty(k)) {
+			continue;
+		}
+		if (typeof g_conf[k] === 'boolean') {
+			$('.'+k).prop('checked', g_conf[k]);
+		}
+		else if (typeof g_conf[k] === 'number') {
+			$('.'+k+'[value='+g_conf[k]+']').prop('checked', g_conf[k]);
+		}
+	}
+
+	let nv = JSON.stringify(g_conf);
+	if (nv !== g_conf_json) {
+		window.localStorage.setItem('config', nv);
+		g_conf_json = nv;
+	}
+}
+
+function dictionaryDelete() {
+	let w = $.trim($(this).closest('form').find('input').val());
+	if (removeFromDictionary(w)) {
+		$(this).closest('form').remove();
+	}
+	else {
+		alert('Kunne ikke slette ordet "'+w+'" fra stavekontrollen!');
+	}
+}
+
+function dictionaryChanged() {
+	let f = $(this).closest('form');
+	let b = f.find('button');
+	if (!b.hasClass('btnWordDelete')) {
+		return;
+	}
+	b.removeClass('btnWordDelete').addClass('btnWordEdit').text('Gem').off().click(function() {
+		b.removeClass('btnWordEdit').addClass('btnWordDelete').html('&times;').off().click(dictionaryDelete);
+		f.submit();
+	});
+}
+
+function attachDictionaryClicks() {
+	$('.formWordEdit').off().submit(function(e) {
+		let ow = $.trim($(this).attr('data-word'));
+		let w = $.trim($(this).find('input').val());
+		if (ow != w && addToDictionary(w) && removeFromDictionary(ow)) {
+			$(this).attr('data-word', w);
+		}
+		else {
+			alert('Kunne ikke ændre ordet "'+ow+'" til '+w+' i stavekontrollen!');
+		}
+
+		e.preventDefault();
+		return false;
+	});
+
+	$('.formWordEdit').find('input').off().keyup(dictionaryChanged).change(dictionaryChanged);
+	$('.btnWordDelete').off().click(dictionaryDelete);
+}
+
+function getState(data) {
+	console.log(data);
+	let s = data.session;
+	// If the locale doesn't exist, trim it and try again
+	if (!l10n.s.hasOwnProperty(s.locale)) {
+		console.log('No such locale ' + s.locale);
+		s.locale = s.locale.replace(/^([^-_]+).*$/, '$1');
+	}
+	// Still doesn't exist, default to Danish
+	if (!l10n.s.hasOwnProperty(s.locale)) {
+		console.log('No such locale ' + s.locale);
+		s.locale = 'da';
+	}
+	session = s;
+
+	loadConfig();
+	loadDictionary();
+
+	$('.words').html('');
+	let ws = Object.keys(g_dictionary);
+	ws.sort();
+	for (let i=0 ; i<ws.length ; ++i) {
+		$('.words').append('<form class="word formWordEdit" data-word="'+escHTML(ws[i])+'"><input type="text" value="'+escHTML(ws[i])+'"> <button type="button" class="btnWordDelete">&times;</button></form>');
+	}
+	attachDictionaryClicks();
+
+	saveConfig();
+	itw_speak_attach(document.body);
+}
+
+function showPane(e, w) {
+	$('.tab.selected').removeClass('selected');
+	$(e).addClass('selected');
+	$('.pane,.column').hide();
+	$('.pane-'+w).show();
+	$('.pane-'+w).find('.sidebar .link').first().click();
+}
+
+function showColumn(e, w) {
+	$('.sidebar .selected').removeClass('selected');
+	$(e).addClass('selected');
+	$('.column').hide();
+	$('.'+w).show();
+}
+
+function initOptions() {
+	$.ajaxSetup({
+		xhrFields: {
+			withCredentials: true,
+		},
+	});
+
+	if (window.location.search.indexOf('tool=Comma') !== -1) {
+		g_tool = 'Comma';
+	}
+	if (g_tool !== 'Grammar' && g_tool !== 'Comma') {
+		g_tool = 'Grammar';
+	}
+	impl_getState();
+
+	$('.closer').click(function() {
+		$(this).closest('.closable').hide();
+	});
+
+	$('.btnOptionsGrammar').click(function() {
+		showColumn(this, 'column-options-grammar');
+	});
+	$('.btnWords').click(function() {
+		showColumn(this, 'column-words');
+	});
+	$('.btnAboutGrammar').click(function() {
+		showColumn(this, 'column-about-grammar');
+	});
+	$('.btnWordsButton').click(function() {
+		$('.btnWords').click();
+	});
+
+	$('.btnOptionsComma').click(function() {
+		showColumn(this, 'column-options-comma');
+	});
+	$('.btnLinks').click(function() {
+		showColumn(this, 'column-links');
+	});
+	$('.btnAboutComma').click(function() {
+		showColumn(this, 'column-about-comma');
+	});
+
+	$('.tab-grammar').click(function() {
+		showPane(this, 'grammar');
+	});
+	$('.tab-comma').click(function() {
+		showPane(this, 'comma');
+	});
+
+	$('.formWordAdd').submit(function(e) {
+		let w = $.trim($('.inputAddWord').val());
+		if (addToDictionary(w)) {
+			$('.words').append('<form class="word formWordEdit" data-word="'+escHTML(w)+'"><input type="text" value="'+escHTML(w)+'"> <button type="button" class="btnWordDelete">&times;</button></form>');
+			attachDictionaryClicks();
+			$('.inputAddWord').val('').focus();
+		}
+		else {
+			alert('Kunne ikke tilføje ordet "'+w+'" til stavekontrollen!');
+		}
+
+		e.preventDefault();
+		return false;
+	});
+
+	$('input[type="checkbox"],input[type="radio"]').change(function() {
+		let k = /(opt_\S+)/.exec($(this).attr('class'));
+		if (!k) {
+			return;
+		}
+
+		k = k[1];
+
+		let v = null;
+		if ($(this).attr('type') === 'checkbox') {
+			v = $(this).prop('checked');
+		}
+		else if ($(this).attr('type') === 'radio') {
+			v = $(this).val();
+		}
+		if (typeof v === 'string' && /^\d+$/.test(v)) {
+			v = parseInt(v);
+		}
+
+		if (k === 'opt_ignUnknown') {
+			g_conf.opt_ignUNames = g_conf.opt_ignUComp = g_conf.opt_ignUAbbr = g_conf.opt_ignUOther = v;
+		}
+
+		g_conf[k] = v;
+		console.log([k, v, g_conf]);
+		saveConfig();
+	});
+
+	$('#error').hide();
+	$('.tab-' + g_tool.toLowerCase()).click();
+	$('#placeholder').remove();
+
+	$('.rpl-vars').each(function() {
+		let e = $(this);
+		if (e.text()) {
+			e.text(e.text().replace('{VERSION}', VERSION));
+		}
+		if (e.attr('src')) {
+			e.attr('src', e.attr('src').replace('{ROOT_URL_SELF}', ROOT_URL_SELF));
+		}
+	});
+}
+
+$(function() {
+	impl_Init(initOptions);
+});
+
+function showError(msg) {
+	console.log(msg);
+	$('#error').show();
+	$('#error-text').text(l10n.t(msg));
+}
