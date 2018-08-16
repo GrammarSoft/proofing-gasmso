3d32c4b1fea8913e046e31e3f80d639e4f748d5a
Tino Didriksen <mail@tinodidriksen.com>
2018-08-16 08:05:45 +0000

    Snip -mso
    git-svn-id: file:///home/komma/repo-gas/svn@97 8336fc70-c169-11e8-9c4d-f55cbf993aad

--- mv-nordic/js/impl-officejs.js
+++ mv-nordic/js/impl-officejs.js
@@ -210,7 +210,7 @@ function _impl_showOptions_cb(asyncResult) {
 }
 
 function impl_showOptions(g_tool) {
-	Office.context.ui.displayDialogAsync(ROOT_URL_SELF + '/html/options-mso.html?tool='+g_tool, { width: 800, height: 600, displayInIframe: true }, _impl_showOptions_cb);
+	Office.context.ui.displayDialogAsync(ROOT_URL_SELF + '/html/options.html?tool='+g_tool, { width: 800, height: 600, displayInIframe: true }, _impl_showOptions_cb);
 }
 
 function _impl_getPars(context, pars) {
