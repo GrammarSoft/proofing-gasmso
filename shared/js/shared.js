7c78fed0e37f8a34d96ef7eefe3b7a20bda63b0b
Tino Didriksen <mail@tinodidriksen.com>
2019-04-12 11:35:53 +0000

    Hotfix for weird quote bug
    git-svn-id: file:///home/komma/repo-gas/svn@146 8336fc70-c169-11e8-9c4d-f55cbf993aad

--- shared/js/shared.js
+++ shared/js/shared.js
@@ -408,6 +408,9 @@ function sanitize_result(txt) {
 	// Special case
 	txt = txt.replace(/@x-etype-case/g, '@upper');
 
+	// Workaround for bug https://trello.com/c/ixmc92EB
+	txt = txt.replace(/.'.\t@proper\n"/g, '.\n"');
+
 	// Swap markers that the backend has mangled due to sentence-ending parentheticals
 	for (let i=0 ; i<Defs.MAX_RQ_SIZE ; ++i) {
 		let t1 = '</s'+i+'>';
