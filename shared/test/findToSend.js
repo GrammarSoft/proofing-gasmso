47853abf2c1e9fa0b256e940df9b28b0c2467559
Tino Didriksen <mail@tinodidriksen.com>
2022-07-28 12:12:26 +0000

    When searching for close context, try a verbatim search to avoid cases where the letters all exist scattered too early
    git-svn-id: file:///home/komma/repo-gas/svn@171 8336fc70-c169-11e8-9c4d-f55cbf993aad

--- shared/test/findToSend.js
+++ shared/test/findToSend.js
@@ -32,6 +32,7 @@ let tests = [
 	{t: 'abc def ghi.', a: ['abc ', 'def', ' ghi. .'], e: {word: 'def'}},
 	{t: 'This program will add missing commas in your English texts and offer advice on superfluous ones as well. Commatizer classifies comma errors into over 30 different categories, and can therefore provide targeted explanations and examples for each suggestion. In addition, explanations are also available for most existing, correct commas. For more information on comma types and errors, please see our English<a href="english_comma.odt">comma manual</a>.', a: ['This program will add missing commas in your English texts and offer advice on superfluous ones as well .  Commatizer classifies comma errors into over 30 different categories ', ',', 'and can therefore provide targeted explanations and examples for each suggestion .  In addition , explanations are also available for most existing , correct commas .  For more information on comma types and errors , please see our English<a href » english_comma.odt » >comma manual  a. '], e: {word: ','}},
 	{ts: ['yuh zyv tyv jkl abc def ghi jkl tyv yuh tyv'], a: ['xuh xyv tyv jkl abc ', 'def', ' ghi jkl tyv xuh xyv'], e: {word: 'def'}},
+	{t: 'Jeg havde planer om at tilbringe eftermiddagen med at fylde min hjerne med så mange græske madrelaterede ord, jeg kunne proppe ind, i forberedelse til aftenen forude. Tidligere samme morgen var Marigold taget forbi landsbybutikken. Hun mente at smart, dekorativt drys af flormelis, kunne være nok til at narre damerne, der samledes på kirkegården, til at tro de var hjemmelavede lækkerier fra hendes egen ovn. Hun vendte tilbage fra butikken med den frygtelige nyhed, at Dina, den ene halvdel af parret, der drev den lokale taverna, havde været så uheldig at falde ned fra en usikkert opstillet oliventræsstige og havde brækket sin arm i faldet.', a: ['Jeg havde planer om at tilbringe eftermiddagen med at fylde min hjerne med så mange græske madrelaterede ord, jeg kunne proppe ind i forberedelse til aftenen forude. Tidligere samme morgen var Marigold taget forbi landsbybutikken. Hun mente at smart, dekorativt drys af flormelis, kunne være nok til at narre damerne, der samledes på kirkegården, til at tro de var hjemmelavede lækkerier fra hendes egen ovn. Hun vendte tilbage fra butikken med den frygtelige nyhed, at Dina, den ene halvdel af parret, der drev den lokale taverna, havde været så uheldig at falde ned fra en usikkert opstillet ', 'oliventræsstige og', ' havde brækket sin arm i faldet.'], e: {word: 'oliventræsstige og'}},
 	];
 
 for (let i=0 ; i<tests.length ; ++i) {
@@ -63,7 +64,9 @@ for (let i=0 ; i<tests.length ; ++i) {
 		if (t.e[k] !== rv[k]) {
 			console.log(`${i+1} FAIL: '${t.e[k]}' !== '${rv[k]}'`);
 		}
-		did = true;
+		else {
+			did = true;
+		}
 	}
 	if (!did) {
 		console.log(`${i+1} FAIL: Returned had no expected fields`);
